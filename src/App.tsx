12df831c07edacd3b307cc93b39a77eef541431a
jiliujiliu <rebichblumkin482@gmail.com>
2026-05-14 17:53:26 +0800

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import ResearchPage from './pages/ResearchPage'
+import AdvisorPage from './pages/AdvisorPage'
+import MembersPage from './pages/MembersPage'
+import PublicationsPage from './pages/PublicationsPage'
+import NewsPage from './pages/NewsPage'
+import RecruitmentPage from './pages/RecruitmentPage'
+import Footer from './components/Footer'
+
+export default function App() {
+  const [scrollProgress, setScrollProgress] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight
+      setScrollProgress(docHeight > 0 ? (scrollTop / docHeight) * 100 : 0)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="scroll-progress" style={{ width: `${scrollProgress}%` }} />
+      <Navbar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/research" element={<ResearchPage />} />
+          <Route path="/advisor" element={<AdvisorPage />} />
+          <Route path="/members" element={<MembersPage />} />
+          <Route path="/publications" element={<PublicationsPage />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/recruitment" element={<RecruitmentPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
