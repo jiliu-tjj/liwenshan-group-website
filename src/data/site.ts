765e959f0abdd121b1321b3bb9e496f0f3ca4038
jiliujiliu <rebichblumkin482@gmail.com>
2026-05-14 17:53:32 +0800

    Add src/data/site.ts

---
+++ src/data/site.ts
@@ -0,0 +1,19 @@
+export const siteConfig = {
+  name: '李文山课题组',
+  nameEn: 'Li Wenshan Research Group',
+  lab: '微纳工程科学全国重点实验室',
+  school: '上海交通大学机械与动力工程学院',
+  schoolShort: 'SJTU ME',
+  address: '上海市闵行区东川路800号上海交通大学机械与动力工程学院',
+  email: 'wenshan.li@sjtu.edu.cn',
+}
+
+export const navItems = [
+  { label: '首页', href: '#/' },
+  { label: '研究方向', href: '#/research' },
+  { label: '导师介绍', href: '#/advisor' },
+  { label: '团队成员', href: '#/members' },
+  { label: '论文成果', href: '#/publications' },
+  { label: '新闻动态', href: '#/news' },
+  { label: '招生信息', href: '#/recruitment' },
+]
